d7de8c0823fa78153675621e0d239cb0161d62b5
codemelinux <codemelinux@gmail.com>
2018-11-21 17:56:07 +0800

    Paypal fix

--- app/src/main/java/com/codemelinux/HUSAP/paypal/PaymentDetails.java
+++ app/src/main/java/com/codemelinux/HUSAP/paypal/PaymentDetails.java
@@ -3,9 +3,12 @@ package com.codemelinux.HUSAP.paypal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.codemelinux.HUSAP.R;
+import com.codemelinux.HUSAP.main.main.NavControl;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,6 +16,7 @@ import org.json.JSONObject;
 public class PaymentDetails extends AppCompatActivity {
 
     TextView txtID, txtAmount, txtStatus;
+    Button returnTOMainPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,15 @@ public class PaymentDetails extends AppCompatActivity {
         txtID = (TextView) findViewById(R.id.textID);
         txtAmount = (TextView) findViewById(R.id.textAmount);
         txtStatus = (TextView) findViewById(R.id.textStatus);
+        returnTOMainPage = (Button) findViewById(R.id.return_main_page);
+
+        returnTOMainPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnPage = new Intent(PaymentDetails.this, NavControl.class);
+                startActivity(returnPage);
+            }
+        });
 
         //get intent
         Intent intent = getIntent();
@@ -32,6 +45,8 @@ public class PaymentDetails extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+
     }
 
     private void showDetails(JSONObject response, String paymentAmount) {
